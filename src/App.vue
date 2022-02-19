8000284278102e402e364455247d756d0052aae1
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-19 13:07:59 +0330

    delete extra assets, styles, components and templates

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Hello</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+  components: {},
 });
 </script>
 
