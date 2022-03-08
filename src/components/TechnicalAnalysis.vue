6d6b0c440bc5a81d0f9ba1decfc411fc8b5f7ea6
eh. sh <sh.ehsan74.es@gmail.com>
2022-03-08 18:31:28 +0330

    refactor TechnicalAnalysis component and make it customizable

--- src/components/TechnicalAnalysis.vue
+++ src/components/TechnicalAnalysis.vue
@@ -11,21 +11,24 @@ export default defineComponent({
   props: {
     options: {
       type: Object,
-      default: () => ({
-        interval: '1m',
-        width: 425,
-        isTransparent: false,
-        height: 450,
-        symbol: 'NASDAQ:AAPL',
-        showIntervalTabs: true,
-        locale: 'en',
-        colorTheme: 'light',
-      }),
+      default: () => ({}),
     },
   },
   setup(props) {
+    const options = {
+      interval: '1m',
+      width: 425,
+      isTransparent: false,
+      height: 450,
+      symbol: 'NASDAQ:AAPL',
+      showIntervalTabs: true,
+      locale: 'en',
+      colorTheme: 'light',
+      ...props.options,
+    };
+
     const { container, tradingview } = useInitWidget(
-      props.options,
+      options,
       'tradingview-technical-analysis',
       'tradingview-technical-analysis-script',
       'https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js'
