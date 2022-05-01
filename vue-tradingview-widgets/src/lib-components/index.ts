6f9825770002d196ccd4f5062d07e06f65c1e40d
eh. sh <sh.ehsan74.es@gmail.com>
2022-05-01 23:04:17 +0430

    add SymbolInfo component to index

--- vue-tradingview-widgets/src/lib-components/index.ts
+++ vue-tradingview-widgets/src/lib-components/index.ts
@@ -17,3 +17,4 @@ export { default as TechnicalAnalysis } from './TechnicalAnalysis.vue';
 export { default as Ticker } from './Ticker.vue';
 export { default as TickerTape } from './TickerTape.vue';
 export { default as MarketOverview } from './MarketOverview.vue';
+export { default as SymbolInfo } from './MarketOverview.vue';
