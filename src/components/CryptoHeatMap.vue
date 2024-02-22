0c7f86595dab0c2d9d380ab005ee8242fe4f793e
Boleigha Mark <donhoenix@gmail.com>
2024-02-22 15:39:49 +0100

    add crypto heatmap (#22)
    * add crypto heatmap
    
    * build new

---
+++ src/components/CryptoHeatMap.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :id="container" ref="tradingview" />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import useInitWidget from '../composable/useInitWidget'
+
+export default defineComponent({
+    name: 'WCryptoHeatMap',
+    props: {
+        options: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+    setup(props: any) {
+        const options = {
+            dataSource: "Crypto",
+            width: 770,
+            height: 400,
+            hasTopBar: false,
+            isDataSetEnabled: false,
+            colorTheme: "dark",
+            locale: 'en',
+            ...props.options,
+        }
+
+        const { container, tradingview } = useInitWidget(
+            options,
+            'tradingview-crypto-heat-map',
+            'tradingview-crypto-heat-map-script',
+            'https://s3.tradingview.com/external-embedding/embed-widget-crypto-coins-heatmap.js'
+        )
+
+        return { container, tradingview }
+    },
+})
+</script>
